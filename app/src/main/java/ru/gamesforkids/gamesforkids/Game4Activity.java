48d11b926295a86ade4133512bbac681775bcea0
Tanya Shelipova <stn.brainstorm@gmail.com>
2018-02-19 13:12:36 +0800

    Игра4: играется

--- app/src/main/java/ru/gamesforkids/gamesforkids/Game4Activity.java
+++ app/src/main/java/ru/gamesforkids/gamesforkids/Game4Activity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,38 +19,40 @@ import java.util.Random;
 
 
 public class Game4Activity extends AppCompatActivity {
-    ImageView imgResult,imgTask;
-    TextView txRed,txBlue,txGreen;
+    ImageView imgResult, imgTask;
+    TextView txRed, txBlue, txGreen;
+    int red, green, blue, acc, RGBtask;
     private SeekBar redSeekBar, greenSeekBar, blueSeekBar;
 
-                @Override
-                protected void onCreate(Bundle savedInstanceState) {
-                    super.onCreate(savedInstanceState);
-                    setContentView(R.layout.activity_game4);
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    String TAG = "tag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game4);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
 
-                    imgResult = (ImageView) findViewById(R.id.imgResult);
-                    imgTask = (ImageView) findViewById(R.id.imgTask);
+        imgResult = (ImageView) findViewById(R.id.imgResult);
+        imgTask = (ImageView) findViewById(R.id.imgTask);
 
-                    txRed = (TextView) findViewById(R.id.txRed);
-                    txGreen = (TextView) findViewById(R.id.txGreen);
-                    txBlue = (TextView) findViewById(R.id.txBlue);
+        txRed = (TextView) findViewById(R.id.txRed);
+        txGreen = (TextView) findViewById(R.id.txGreen);
+        txBlue = (TextView) findViewById(R.id.txBlue);
 
-                    redSeekBar = (SeekBar) findViewById(R.id.seekBarRed);
-                    greenSeekBar = (SeekBar) findViewById(R.id.seekBarGreen);
-                    blueSeekBar = (SeekBar) findViewById(R.id.seekBarBlue);
-                    updateBackground();
+        redSeekBar = (SeekBar) findViewById(R.id.seekBarRed);
+        greenSeekBar = (SeekBar) findViewById(R.id.seekBarGreen);
+        blueSeekBar = (SeekBar) findViewById(R.id.seekBarBlue);
+        updateBackground();
 
-                    redSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
-                    greenSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
-                    blueSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+        redSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+        greenSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+        blueSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
 
+        acc = 30; // точность
+        newGame();
 
-//для проверки - нормально заполняет
-                    imgTask.setColorFilter(0xff00ffff);
-
-                }
+    }
 
     private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
         @Override
@@ -67,7 +71,25 @@ public class Game4Activity extends AppCompatActivity {
         }
     };
 
-                //изменение значений бегунков
+    private void newGame() {
+        Random rnd = new Random();
+        red = rnd.nextInt((255 - 0) + 1) + 0;
+        green = rnd.nextInt((255 - 0) + 1) + 0;
+        blue = rnd.nextInt((255 - 0) + 1) + 0;
+
+        redSeekBar.setProgress(150);
+        greenSeekBar.setProgress(150);
+        blueSeekBar.setProgress(150);
+
+        Log.i(TAG,String.valueOf(red));
+        Log.i(TAG,String.valueOf(green));
+        Log.i(TAG,String.valueOf(blue));
+        RGBtask = android.graphics.Color.rgb(red, green, blue);
+        imgTask.setColorFilter(RGBtask);
+        imgResult.setColorFilter(android.graphics.Color.rgb(150, 150, 150));
+    }
+
+    //изменение значений бегунков
     private void updateBackground() {
         int redValue, greenValue, blueValue;
         redValue = redSeekBar.getProgress();
@@ -77,6 +99,31 @@ public class Game4Activity extends AppCompatActivity {
         txRed.setText(Integer.toString(redValue));
         txGreen.setText(Integer.toString(greenValue));
         txBlue.setText(Integer.toString(blueValue));
+
+        int RGB = android.graphics.Color.rgb(redValue, greenValue, blueValue);
+
+        imgResult.setColorFilter(RGB);
+
+        if (((redValue > red - acc) & (redValue < red + acc))
+        & ((greenValue > green - acc) & (greenValue < green + acc))
+        & ((blueValue > blue - acc) & (blueValue < blue + acc))){
+            Toast.makeText(getApplicationContext(), "УРА", Toast.LENGTH_LONG).show();
+            imgResult.setColorFilter(RGBtask);
+            redSeekBar.setEnabled(false);
+            greenSeekBar.setEnabled(false);
+            blueSeekBar.setEnabled(false);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    newGame();
+                    redSeekBar.setEnabled(true);
+                    greenSeekBar.setEnabled(true);
+                    blueSeekBar.setEnabled(true);
+                }
+            }, 3000);
+
+
+        }
 
     }
 }
